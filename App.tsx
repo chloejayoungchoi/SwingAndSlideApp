52b06e9ba7fa68359738c7e929df5ebd5952eed8
chloejayoungchoi <cchoi6484@gmail.com>
2023-08-22 10:51:54 -0700

    현재위치 가져오기

--- App.tsx
+++ App.tsx
@@ -10,6 +10,9 @@ import { useEffect,
          useRef } from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import { Linking } from 'react-native';
+import { Platform, PermissionsAndroid } from "react-native";
+import Geolocation from "react-native-geolocation-service";
+
 
 const App = () => {
   const BASE_URL = 'https://swingandslide.net';
@@ -32,7 +35,7 @@ const App = () => {
       'hardwareBackPress',
       () => {
         if (isGalleryActive) {
-          webview.current.postMessage(JSON.stringify({type: "CALL", value: "CLOSE_GALLERY"}));
+          sendMessageToWebview("CALL", "CLOSE_GALLERY");
           return true;
         }
 
@@ -59,6 +62,7 @@ const App = () => {
   };
   */
 
+  // 웹뷰에서 메시지 수신
   const onHandleMessageFromWebview = (e) => {
     try{
       const data = JSON.parse(e.nativeEvent.data)
@@ -70,10 +74,59 @@ const App = () => {
         case "GALLERY" :
           setIsGalleryActive(data.value);
           break;
+        case "REQ" :
+          requestPermission(data.value);
+          break; 
       }
     }catch(e) { 
       console.log(e)
     }
+  }
+
+
+  // 위치 퍼미션 얻기
+  async function requestPermission(per) {
+    let permissionString = null;
+    let permissionResult = null;
+    switch (per) {
+      case "REQ_ACCESS_FINE_LOCATION" :
+        permissionString = PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION;
+        break;
+    }
+  
+    try {
+      if (Platform.OS === "ios") {
+        permissionResult = await Geolocation.requestAuthorization("always");
+      }
+      // 안드로이드 위치 정보 수집 권한 요청
+      if (Platform.OS === "android") {
+        permissionResult = await PermissionsAndroid.request(permissionString);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    // 퍼미션 허용 후 웹뷰에 메시지 
+    if(permissionResult === "granted") {
+      Geolocation.getCurrentPosition(
+        pos => {
+          sendMessageToWebview("GRANTED", {lat:pos.coords.latitude, lng:pos.coords.longitude})
+        },
+        error => {
+          console.log(error);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 3600,
+          maximumAge: 3600,
+        },
+      );
+    }
+
+  }
+  
+  function sendMessageToWebview(type, value) {
+    webview.current.postMessage(JSON.stringify({'type': type, 'value': value}));
   }
 
   return (
@@ -86,14 +139,14 @@ const App = () => {
           allowsBackForwardNavigationGestures={true}
           source={{ uri: BASE_URL }}
           onMessage={onHandleMessageFromWebview}
-          onShouldStartLoadWithRequest={event => {
-            //외부링크
-            if (!event.url.startsWith(BASE_URL)) {
-              Linking.openURL(event.url);
-              return false;
-            }
-            return true;
-          }}
+          // onShouldStartLoadWithRequest={event => {
+          //   //외부링크
+          //   if (!event.url.startsWith(BASE_URL)) {
+          //     Linking.openURL(event.url);
+          //     return false;
+          //   }
+          //   return true;
+          // }} // 안드로이드에서는 괜찮은데 ios에서 문제가 생겨서 주석처리함 230822
           injectedJavaScript={`
             (function() {
                 function wrap(fn) {
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  SwingAndSlideApp
+//
+//  Created by JAYOUNG CHOI on 2023-08-20.
+//
+
+import Foundation
